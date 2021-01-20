2e88499937e5cbe4d4a37cbdfcc2536b5257412f
LeeJeaWon <73148498+LeeJeaWon@users.noreply.github.com>
2021-01-20 16:49:10 +0900

    JS: navbar

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+// navbar 화면고정
+const navbar = document.querySelector('#navbar')
+const navbarHeight = navbar.getBoundingClientRect().height
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY >navbarHeight ){
+        navbar.classList.add('navbar--dark')
+    }else{
+        navbar.classList.remove('navbar--dark')
+    }
+})
+
