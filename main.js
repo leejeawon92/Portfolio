98dd8df0cac2d6cbeb13b161a448af3ccd1b722b
LeeJeaWon <73148498+LeeJeaWon@users.noreply.github.com>
2021-01-21 10:32:06 +0900

    JS: navbar menu scroll

--- main.js
+++ main.js
@@ -2,14 +2,30 @@
 
 
 // navbar 화면고정
-const navbar = document.querySelector('#navbar')
-const navbarHeight = navbar.getBoundingClientRect().height
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
     if(window.scrollY >navbarHeight ){
-        navbar.classList.add('navbar--dark')
+        navbar.classList.add('navbar--dark');
     }else{
-        navbar.classList.remove('navbar--dark')
+        navbar.classList.remove('navbar--dark');
     }
+});
+
+// navbar item들 클릭했을때 scrolling
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView();
 })
+
 
