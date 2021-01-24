8ad9f8170d30bb9c34c53919e98cd0f5a04c9053
LeeJeaWon <73148498+LeeJeaWon@users.noreply.github.com>
2021-01-24 20:49:53 +0900

    JS: select navbar + 최종 완료

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ navbarMenu.addEventListener('click', (event) => {
     }
     navbarMenu.classList.remove('open');   
     scrollIntoView(link);
+    selectNavItem(target);
 })
 
 // =====================================================================================================
@@ -41,10 +42,6 @@ homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 })
 
-function scrollIntoView(selector){
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView();
-}
 
 
 // =====================================================================================================
@@ -72,7 +69,6 @@ arrowUp.addEventListener('click', () => {
 
 // =====================================================================================================
 // Project
-
 const workBtnContainer = document.querySelector('.work__categories')
 const projectContainer = document.querySelector('.work__projects')
 const projects = document.querySelectorAll('.project')
@@ -102,4 +98,98 @@ workBtnContainer.addEventListener('click', (e) =>{
     }, 300)
     
 });
+
+// =====================================================================================================
+// Project
+//1. 모든 section 요소들과 메뉴아이템들을 가져온다 
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#contact'
+]
+
+const sections = sectionIds.map(id => document.querySelector(id))
+// console.log(sections)
+
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`))
+// console.log(navItems)
+
+
+//2. intersectionObserver를 이용해서 모든 section들을 관찰한다.
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active')
+    selectedNavItem = selected
+    selectedNavItem.classList.add('active')
+}
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView();
+    selectNavItem(navItems[sectionIds.indexOf(selector)])
+}
+
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3
+}
+
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry=>{
+        if(!entry.isIntersecting && entry.intersectionRatio >0) { //스크롤을 위로 올렸을때()
+            const index = sectionIds.indexOf(`#${entry.target.id}`)
+
+            if(entry.boundingClientRect.y < 0){
+                // 스크롤이 아래로 되어서 페이지가 올라옴
+                selectedNavIndex = index+1
+            } else{
+                selectedNavIndex = index-1
+            }
+        }
+    })
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions)
+sections. forEach(section => observer.observe(section))
+
+
+window.addEventListener('wheel', () => {
+    if(window.scrollY === 0 ) {
+        selectedNavIndex = 0;
+    } else if (Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeight){
+        selectedNavIndex = navItems.length-1;
+    }
+    selectNavItem(navItems[selectedNavIndex])
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
